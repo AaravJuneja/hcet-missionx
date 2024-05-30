f7fd3c88aacc5c205ca23587d07f1abdabfb7ce8
Aarav Juneja (He/Him) <aarav.juneja@ais.amity.edu>
2024-05-30 21:59:46 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -73,7 +73,7 @@ app.get('/frontend/:file', async (req, res) => {
 
 // Start the server
 app.listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+    console.log('Server is running on https://backend-jvpf.onrender.com/');
 });
 
 app.use((req, res, next) => {
